7660f8995c4fb501539fb0e5bb42fd381cf4fb4a
Diep Ho <ho25l@mtholyoke.edu>
2022-06-20 15:41:25 +0700

    modify

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,5 @@
 from transformers import pipeline
 import pandas as pd
-# import nltk
-# from nltk.tokenize import sent_tokenize
-# from transformers import AutoTokenizer
 
 
 def extractingData(fileName):
@@ -14,23 +11,14 @@ def extractingData(fileName):
 def test_summarizer(text):
     
     summarizer = pipeline("summarization", model="t5-large")
-    # summaries = {}
     output = summarizer(text, max_length=512)
-    # summaries["t5"] = "\n".join(sent_tokenize(pipe_out[0]["summary_text"]))
 
     return output
-
-# def summarizer(text):
-#     summarizer = pipeline("summarization")
-#     outputs = summarizer(text, max_length=512, clean_up_tokenization_spaces=True)
-#     return outputs
-
 
 def strSummary(summary):
     strSummary = summary[0]['summary_text']
     return strSummary
 
-# print(summarizer(extractingData("test.csv")))
 
 def summary_main(fileName):
     text = extractingData(fileName)
