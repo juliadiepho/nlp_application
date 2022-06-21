63480b21aa5728bdeb24ca314730c6e3e297bf4c
Diep Ho <ho25l@mtholyoke.edu>
2022-06-21 14:17:26 +0700

    add functions for daily & weekly detector

--- emotion_detector.py
+++ emotion_detector.py
@@ -5,26 +5,20 @@ import pandas as pd
 import nltk
 
 
-def emotion_detector(text):
+
+def daily_emotion_detector(text):
     emotion = pipeline("sentiment-analysis", model="arpanghoshal/EmoRoBERTa")
     emotion_labels = emotion(text)
 
     return emotion_labels
 
+def weekly_emotion_detector(text):
+    emotion = pipeline("sentiment-analysis", model="arpanghoshal/EmoRoBERTa")
+    
+    return text.apply(emotion)
+
 # text = strSummary(test_summarizer(extractingData("test.csv"))
 # print(emotion_detector(text))Date,Content
-
-
-
-# prefix = "summarize: "
-# def preprocess_function(fileName):
-#     inputs = [prefix + doc for doc in fileName[0]]
-#     model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
-
-#     with tokenizer.as_target_tokenizer():
-#         labels = tokenizer(fileName["summary"], max_length=128, truncation=True)
-
-#     model_inputs["labels"] = labels["input_ids"]
-#     return model_inputs
-# print(preprocess_function("test.csv"))
+# text = extractingData("test.csv")
+# print(text.apply(emotion))
 
