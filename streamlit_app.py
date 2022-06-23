9be4ce67fa07c7353dce02a19b08672486b66281
Diep Ho <ho25l@mtholyoke.edu>
2022-06-23 15:07:38 +0700

    create streamlit file

---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import csv
+
+st.title("Welcome to Digital Journal!")
+st.markdown("This is where you can write your online dairy, get reports on how you feel daily and weekly, and get summarization of your day and week.")
+
+def ask_input_diary():
+    ask_file = st.radio("Do you want to create a new diary?", ["Yes", "No"])
+    if ask_file == "Yes":
+        file_name = st.text_input("Name of your new diary:")
+    else:
+        file_name = st.multiselect("Choose an existing diary", ["Diary_1", "Diary_2"])
+    
+    cont = st.button('Continue')
+    if cont == True:
+        ask_input()
+    
+    return file_name
+
+
+
+
+def ask_input():
+    date_input = st.date_input("Choose the date that you want to write about")
+    # st.write(date_input)
+    user_input = st.text_area("How was your day?")
+    data_content = [date_input, user_input]
+    return data_content
+
+# user_input()
+data_header = ["Date", "Content"]
+
+def file_writer (file_name):
+
+
+    try:
+        with open(file_name, "a") as file:
+            writer = csv.writer(file)
+            writer.writerow(ask_input())
+    except:
+        with open(file_name, "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(data_header)
+            writer.writerow(ask_input())
+
+ask_input_diary()
