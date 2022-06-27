3f6dcca08131bb6a7d77976b8651afbb7bac5341
Diep Ho <ho25l@mtholyoke.edu>
2022-06-27 10:23:46 +0700

    modify summarizer functions

--- summarizer.py
+++ summarizer.py
@@ -1,22 +1,43 @@
 from transformers import pipeline
 import pandas as pd
+import streamlit as st
 
+# def extracting_data(fileName):  
+#     user_input = input("Do you want to generate daily or weekly report? [d/w]: ")
+#     if user_input == "d": 
+#         date = input("What is the date that you want to generate report on?: ")
+#         data = pd.read_csv(fileName)
+#         for i in range(len(data)):
+#             if data['Date'][i] == date:
+#                 text = data['Content'][i]
+#                 return text
+#     elif user_input == "w":
+#         data = pd.read_csv(fileName)
+#         text = data['Content'][:6]
+#         return text
+#     else:
+#         return "Please enter a valid input [d/w]"
 
-def extractingData(fileName):  
-    user_input = input("Do you want to generate daily or weekly report? [d/w]: ")
-    if user_input == "d": 
-        date = input("What is the date that you want to generate report on?: ")
-        data = pd.read_csv(fileName)
+    
+def extracting_data_summary(user_input, file_name):  
+    #user_input = st.radio("Do you want to generate daily or weekly summarization?", ["Daily", "Weekly"])
+    if user_input == "Daily": 
+        data = pd.read_csv(file_name)
+        date_list = []
+
         for i in range(len(data)):
-            if data['Date'][i] == date:
+            date_list.append(data['Date'][i])
+        
+        date_option = st.selectbox("Choose a date", date_list, key="s")
+        for i in range(len(data)):
+            if data['Date'][i] == date_option:
                 text = data['Content'][i]
                 return text
-    elif user_input == "w":
-        data = pd.read_csv(fileName)
+
+    elif user_input == "Weekly":
+        data = pd.read_csv(file_name)
         text = data['Content'][:6]
         return text
-    else:
-        return "Please enter a valid input [d/w]"
 
 def test_summarizer(text):
     
@@ -29,12 +50,12 @@ def str_summary(summary):
     str_summary = summary[0]['summary_text']
     return str_summary
 
-def summary_main(fileName):
-    text = extractingData(fileName)
-    summarized = test_summarizer(text)
-    summarized_final = str_summary(summarized)
+# def summary_main(fileName):
+#     text = extracting_data(fileName)
+#     summarized = test_summarizer(text)
+#     summarized_final = str_summary(summarized)
 
-    return summarized_final
+#     return summarized_final
 
 #print(summary_main("test.csv"))
 #print(extractingData("test.csv"))
