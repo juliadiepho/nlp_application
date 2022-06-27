50b02b47533a0813526c63621fc34555532dad55
Diep Ho <ho25l@mtholyoke.edu>
2022-06-27 10:23:03 +0700

    modify emotion functions

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,9 +1,30 @@
 from matplotlib.pyplot import get
 from numpy import extract
 from transformers import pipeline
-from summarizer import test_summarizer, extractingData, str_summary
+from summarizer import test_summarizer, str_summary
 import pandas as pd
+import streamlit as st
 # import nltk
+
+def extracting_data_emotion(user_input, file_name):  
+    #user_input = st.radio("Do you want to generate daily or weekly summarization?", ["Daily", "Weekly"])
+    if user_input == "Daily": 
+        data = pd.read_csv(file_name)
+        date_list = []
+
+        for i in range(len(data)):
+            date_list.append(data['Date'][i])
+        
+        date_option = st.selectbox("Choose a date", date_list, key="e")
+        for i in range(len(data)):
+            if data['Date'][i] == date_option:
+                text = data['Content'][i]
+                return text
+
+    elif user_input == "Weekly":
+        data = pd.read_csv(file_name)
+        text = data['Content'][:6]
+        return text
 
 def daily_emotion_detector(text):
     emotion = pipeline("sentiment-analysis", model="arpanghoshal/EmoRoBERTa")
