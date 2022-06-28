394d5b3e4369f2220e155c3ac19a23b5e87f4bce
Diep Ho <ho25l@mtholyoke.edu>
2022-06-28 14:31:55 +0700

    modify file writer function to print header to an empty file

--- file_writer.py
+++ file_writer.py
@@ -1,6 +1,7 @@
 import csv
 import streamlit as st
 import pandas as pd
+import os
 
 data_header = ["Date", "Content"]
 
@@ -9,17 +10,22 @@ def ask_input():
         date_input = st.date_input("Enter a date")
         user_input = st.text_area("How was your day?")
         data_content = [date_input, user_input]
+    
+    return data_content
              
 
 def ask_input_diary():
     ask_file = st.radio("Do you want to create a new diary?", ["Yes", "No"])
-    
+
     if ask_file == "Yes":
         file_name = st.text_input("Name of your new diary:")
+        new_file = [file_name]
         if file_name != "":
             with open("diary_storage.csv", "a") as file:
                 writer = csv.writer(file)
-                writer.writerow(file_name)
+                writer.writerow(new_file)
+        elif file_name == "":
+            st.error("Please enter a name for your new diary")
     else:
         diaries = pd.read_csv("diary_storage.csv")
         diary_list = []
@@ -31,15 +37,22 @@ def ask_input_diary():
     
     return file_name
 
-def file_writer (file_name, data_content):
-    try:
-        with open(file_name, "a") as file:
-            writer = csv.writer(file)
-            writer.writerow(data_content)
-    except:
-        with open(file_name, "w") as file:
-            writer = csv.writer(file)
+def file_writer (file_name, data_content, data_header):
+    with open(file_name, "a") as file:
+        writer = csv.writer(file)
+        if os.stat(file_name).st_size == 0:
             writer.writerow(data_header)
-            writer.writerow(file_name, data_content)
+            # writer.writerow(data_content)
+        else:
+           writer.writerow(data_content) 
+
+        writer = csv.writer(file)
+        writer.writerow(data_content)
+    # except:
+    #     with open(file_name, "w") as file:
+    #         data_header = ["Date", "Content"]
+    #         writer = csv.writer(file)
+    #         writer.writerow(data_header)
+    #         writer.writerow(data_content)
 
 # file_writer("test.csv")
